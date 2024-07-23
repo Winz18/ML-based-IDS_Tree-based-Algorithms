523cc5dde1c601a4f93ac7be1fbfebcb7267a742
Winz18 <117364628+Winz18@users.noreply.github.com>
2024-07-23 14:26:19 +0700

    fix bugs in model.py | 23/07/24

--- model.py
+++ model.py
@@ -8,10 +8,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.tree import DecisionTreeClassifier
 import os
-import logging
 
-# Cài đặt logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Hằng số
 RANDOM_STATE = 42
@@ -20,10 +17,9 @@ SMOTE_SAMPLING_STRATEGY = {4: 1500}
 N_ESTIMATORS = 500
 DATA_FILE_PATH = 'MachineLearningCSV/MachineLearningCVE/data_processed.csv'
 
-
 def load_and_clean_data(file_path):
     if not os.path.exists(file_path):
-        logging.error(f"File not found: {file_path}")
+        print(f"File not found: {file_path}")
         return None, None
 
     df = pd.read_csv(file_path)
@@ -40,7 +36,6 @@ def load_and_clean_data(file_path):
 
     return df, numeric_features
 
-
 def preprocess_and_split_data(df, numeric_features):
     scaler = StandardScaler()
     df[numeric_features] = scaler.fit_transform(df[numeric_features])
@@ -51,38 +46,31 @@ def preprocess_and_split_data(df, numeric_features):
     X = df.drop(columns=[' Label'], axis=1)
     y = df[' Label']
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_SIZE, random_state=RANDOM_STATE,
-                                                        stratify=y)
-    logging.info(f"Original training set class distribution: {y_train.value_counts()}")
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_SIZE, random_state=RANDOM_STATE, stratify=y)
+    print(f"Original training set class distribution: {y_train.value_counts()}")
 
     smote = SMOTE(random_state=RANDOM_STATE, n_jobs=-1, sampling_strategy=SMOTE_SAMPLING_STRATEGY)
     X_train, y_train = smote.fit_resample(X_train, y_train)
-    logging.info(f"Resampled training set class distribution: {y_train.value_counts()}")
+    print(f"Resampled training set class distribution: {y_train.value_counts()}")
 
     return X_train, X_test, y_train, y_test, scaler, le
 
-
 def train_model(X_train, y_train):
-    rf = RandomForestClassifier(random_state=RANDOM_STATE, oob_score=True, n_jobs=-1, class_weight="balanced_subsample",
-                                n_estimators=N_ESTIMATORS)
+    rf = RandomForestClassifier(random_state=RANDOM_STATE, oob_score=True, n_jobs=-1, class_weight="balanced", n_estimators=N_ESTIMATORS)
     dt = DecisionTreeClassifier(random_state=RANDOM_STATE, class_weight="balanced")
-    et = ExtraTreesClassifier(random_state=RANDOM_STATE, n_jobs=-1, class_weight="balanced_subsample",
-                              n_estimators=N_ESTIMATORS)
+    et = ExtraTreesClassifier(random_state=RANDOM_STATE, n_jobs=-1, class_weight="balanced", n_estimators=N_ESTIMATORS)
 
-    voting_model = VotingClassifier(estimators=[('rf', rf), ('dt', dt), ('et', et)], voting='hard', n_jobs=-1,
-                                    verbose=True)
+    voting_model = VotingClassifier(estimators=[('rf', rf), ('dt', dt), ('et', et)], voting='hard', n_jobs=-1, verbose=True)
     voting_model.fit(X_train, y_train)
 
     return voting_model
 
-
 def evaluate_model(model, X_test, y_test, le):
     y_pred = model.predict(X_test)
-    logging.info("Confusion Matrix:")
-    logging.info(confusion_matrix(y_test, y_pred))
-    logging.info("Classification Report:")
-    logging.info(classification_report(y_test, y_pred, target_names=le.classes_))
-
+    print("Confusion Matrix:")
+    print(confusion_matrix(y_test, y_pred))
+    print("Classification Report:")
+    print(classification_report(y_test, y_pred, target_names=le.classes_))
 
 def feature_importance_optimized(model, X_train):
     feature_importances = np.mean([est.feature_importances_ for est in model.estimators_], axis=0)
@@ -93,18 +81,15 @@ def feature_importance_optimized(model, X_train):
     features_df['cumulative_importance'] = features_df['importance'].cumsum()
     selected_features = features_df[features_df['cumulative_importance'] <= 0.9]['feature'].tolist()
 
-    logging.info(features_df)
-    logging.info(f"Total number of features: {len(features_df)}")
-    logging.info(f"Selected features: {selected_features}")
+    print(f"Total number of selected features: {len(selected_features)}")
+    print(f"Selected features: {selected_features}")
 
     return selected_features
-
 
 def save_models(model, scaler, le):
     joblib.dump(model, 'voting_model.joblib')
     joblib.dump(scaler, 'scaler.joblib')
     joblib.dump(le, 'label_encoder.joblib')
-
 
 if __name__ == '__main__':
     df, numeric_features = load_and_clean_data(DATA_FILE_PATH)
@@ -114,8 +99,7 @@ if __name__ == '__main__':
         evaluate_model(voting_model, X_test, y_test, le)
 
         selected_features = feature_importance_optimized(voting_model, X_train)
-        X_train, X_test, y_train, y_test = train_test_split(df[selected_features], df[' Label'], test_size=TEST_SIZE,
-                                                            random_state=RANDOM_STATE, stratify=df[' Label'])
+        X_train, X_test, y_train, y_test = train_test_split(df[selected_features], df[' Label'], test_size=TEST_SIZE, random_state=RANDOM_STATE, stratify=df[' Label'])
         voting_model = train_model(X_train, y_train)
         evaluate_model(voting_model, X_test, y_test, le)
 
